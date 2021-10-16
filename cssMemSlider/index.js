ccbb987ac8c0500803fd4f7d43365a3606d4e292
AlexGrin1 <a_grynevich@mail.ru>
2021-10-17 00:48:21 +0300

    feat: finished slider

--- cssMemSlider/index.js
+++ cssMemSlider/index.js
@@ -1,16 +1,28 @@
 const controlls = document.querySelector(".slider-controll_panel");
 const allButtons = document.querySelectorAll(".slider-controll_button");
 const slidersImages = document.querySelectorAll(".slider-block img");
-console.log(slidersImages);
+const allDescriptions = document.querySelectorAll(".description");
 controlls.addEventListener("click", (ev) => {
-  let eventButton = ev.target;
-  allButtons.forEach((el) => el.classList.remove("active"));
-  if (eventButton.tagName == "LI") {
-    eventButton.classList.add("active");
-  }
+  if (ev.target.tagName != "UL") {
+    let eventButton = ev.target;
+    if (eventButton.tagName == "LI") {
+      allButtons.forEach((el) => el.classList.remove("active"));
+      eventButton.classList.add("active");
+    }
+    const activeButton = ev.target.getAttribute("data-number");
+    const activeSlide = document.querySelector(`[data-value='${activeButton}']`);
+    const activeDescription = document.querySelector(`[data-description='${activeButton}']`);
 
-  const activeButton = ev.target.getAttribute("data-number");
-  const activeSlide = document.querySelector(`[data-value='${activeButton}']`);
-  slidersImages.forEach((el) => el.classList.remove("active-slide"));
-  activeSlide.classList.add("active-slide");
+    slidersImages.forEach((el) => el.classList.remove("active-slide"));
+
+    activeSlide.classList.remove("hidden");
+    activeSlide.classList.add("active-slide");
+    // activeSlide.classList.add("slide-hidden-effect");
+    activeDescription.classList.add("hidden-effect");
+    allDescriptions.forEach((el) => el.classList.remove("active-description"));
+    setTimeout(function () {
+      activeDescription.classList.remove("hidden-description");
+      activeDescription.classList.add("active-description");
+    }, 300);
+  }
 });
